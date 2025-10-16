956b759ec34e1b6d19568e773d2db8d0e31aa637
sunamiao <112919421+sunamiao@users.noreply.github.com>
2025-10-17 00:09:22 +0800

    update test

--- main.cpp
+++ main.cpp
@@ -10,11 +10,14 @@ int main() {
     clock_t start = clock();
 
     vector<int> results = twoSum(nums,target);
+
     clock_t end = clock();
     double cpu_time_used = static_cast<double>(end - start) / CLOCKS_PER_SEC;
     std::cout << "CPU time used: " << cpu_time_used << " s\n";
+
     for(auto result:results)
     cout << result;
+
     std::cout << time(0) << endl;
     return 0;
 }
